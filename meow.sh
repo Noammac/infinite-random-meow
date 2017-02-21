828a236bcf5646a7367437a3b8958523886dfc8b
Noam Barnea <noammac.yuval2@gmail.com>
2017-02-21 16:35:06 +0200

    Removed outside random selector, made speech more random Made the amplitude, pitch, and speed of meows more random

---
+++ meow.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+while true
+	do
+		amp=`shuf -i 25-200 -n 1`
+		pitch=`shuf -i 0-99 -n 1`
+		speed=`shuf -i 0-450 -n 1`
+		meow=`yes "meow " | head -n $(shuf -i 1-10 -n 1)`
+		echo "amp = $amp, pitch = $pitch, speed = $speed"
+		echo $(echo -e "${meow}" | tr "\n" " ")
+		espeak -a $amp -p $pitch -s $speed $meow
+done
