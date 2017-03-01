f419bcbb0851aa6b1378452ed9ce9356ef5db192
Noam Barnea <noammac.yuval2@gmail.com>
2017-03-01 22:25:20 +0200

    Created Windows port 'meow.ps1'; updated README.md

---
+++ meow.ps1
@@ -0,0 +1,10 @@
+while(1) {
+	$pitch = $(Get-Random -minimum -10 -maximum 10)
+	$speed = $(Get-Random -minimum -10 -maximum 10)
+	$volume = $(Get-Random -minimum 1 -maximum 100)
+	$meow = "meow " * $(Get-Random -minimum 1 -maximum 10)
+	write-host "Pitch: $pitch, Speed: $speed, Volume: $volume"
+    write-host $meow
+	$path = (Get-ItemProperty "hklm:SOFTWARE\MG STUDIO\SPEECHMG").InstallPath
+	& ($path + "speechmg") $meow $pitch $speed $volume
+}
