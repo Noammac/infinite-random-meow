3686fc51a68a7c8beb740cce5090db28ca7c42bf
Noam Barnea <noammac.yuval2@gmail.com>
2017-04-24 18:50:31 +0300

    Fixed bug where each iteration would only speak one meow

--- meow.sh
+++ meow.sh
@@ -7,5 +7,5 @@ while true
 		meow=`yes "meow " | head -n $(shuf -i 1-10 -n 1)`
 		echo "amp = $amp, pitch = $pitch, speed = $speed"
 		echo $(echo -e "${meow}" | tr "\n" " ")
-		espeak -a $amp -p $pitch -s $speed $meow
+		espeak -a $amp -p $pitch -s $speed "$meow"
 done
